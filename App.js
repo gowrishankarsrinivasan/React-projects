0152e6cf3ffdd7221b58e99dee2663e63ff35e86
gowrishankarsrinivasan <152590460+gowrishankarsrinivasan@users.noreply.github.com>
2023-12-01 12:46:06 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+// import './App.css';
+import Example from './components/demo';
+
+
+
+import Table1 from './components/table';
+
+function App() {
+  return (
+    <div className="App">
+    <Example/>
+    </div>
+  );
+}
+
+export default App;
