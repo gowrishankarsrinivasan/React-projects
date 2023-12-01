df0070c163e2b4e7a957d26096547805e08064b3
gowrishankarsrinivasan <152590460+gowrishankarsrinivasan@users.noreply.github.com>
2023-12-01 12:49:15 +0530

    Add files via upload

---
+++ components/demo.js
@@ -0,0 +1,12 @@
+import React,{useEffect} from 'react';
+
+function Example() {
+    useEffect(() => {
+        console.log('example................................................');
+    });
+
+    return (
+        <h1>Hi</h1>
+    )
+}
+export default Example;
